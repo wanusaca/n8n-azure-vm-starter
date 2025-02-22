229a948fa8f8d08a5aca01790c1031a22e330d0c
Jawand Singh <virk.jawand@gmail.com>
2025-02-22 20:59:06 +0530

    updated documentation and deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ LOCATION="eastus"
 VM_NAME="n8n-vm"
 ADMIN_USERNAME="n8nadmin"
 DNS_PREFIX="n8n-$(date +%s | cut -c6-10)"
-DOMAIN_NAME="n8n.prismantis.com"
 
 # Create resource group
 az group create --name $RESOURCE_GROUP --location $LOCATION
@@ -24,13 +23,12 @@ az deployment group create \
     vmName=$VM_NAME \
     adminUsername=$ADMIN_USERNAME \
     adminPasswordOrKey="$(cat ~/.ssh/id_rsa_n8n.pub)" \
-    dnsLabelPrefix=$DNS_PREFIX \
-    n8nDomain=$DOMAIN_NAME
+    dnsLabelPrefix=$DNS_PREFIX
 
 # Get the VM's public IP
 VM_IP=$(az vm show -d -g $RESOURCE_GROUP -n $VM_NAME --query publicIps -o tsv)
 
 # Output connection information
 echo "VM deployed successfully!"
-echo "SSH connection: ssh $ADMIN_USERNAME@$VM_IP"
+echo "SSH connection: ssh -i ~/.ssh/id_rsa_n8n $ADMIN_USERNAME@$VM_IP"
 echo "DNS name: $DNS_PREFIX.$LOCATION.cloudapp.azure.com" 
