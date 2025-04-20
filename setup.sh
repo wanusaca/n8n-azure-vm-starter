b8b096d9d5d1992d03f4baa2bc432cca0ade4189
wanusaca <149994055+wanusaca@users.noreply.github.com>
2025-04-20 14:04:11 -0600

    Update setup.sh
    Replaced hardcoded Docker Compose with latest version.

--- setup.sh
+++ setup.sh
@@ -12,7 +12,10 @@ sudo usermod -aG docker $USER
 # Notify about group changes
 echo "Added user to docker group. You may need to log out and back in for this to take effect."
 
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.24.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Get the latest version of Docker Compose
+DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Install Caddy for HTTPS
